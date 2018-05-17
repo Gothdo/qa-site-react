fdcc8513e8773ad5d2809cf2031639bc7983075f
Michał Perłakowski <michal@perlakowski.pl>
2018-05-17 11:29:32 +0200

    Fix imports in Question

--- src/Question.jsx
+++ src/Question.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import Card, { CardContent } from '@material-ui/core/Card';
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
 import Typography from '@material-ui/core/Typography';
 import Grid from '@material-ui/core/Grid';
 import Tooltip from '@material-ui/core/Tooltip';
